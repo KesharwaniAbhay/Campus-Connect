c8fb1f865fab5f9b7c115e9316dcf3e5505e07a0
Abhay Kesharwani <55699875+KesharwaniAbhay@users.noreply.github.com>
2024-11-27 22:53:01 +0530

    Update data.js

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -1,7 +1,8 @@
 export const categories = [
-    { id: 1, type: "Music" },
-    { id: 2, type: "Movies" },
-    { id: 3, type: "Sports" },
-    { id: 4, type: "Tech" },
-    { id: 5, type: "Fashion" }
+   { id: 1, type: "CSED" },
+    { id: 2, type: "Mechanical Dept" },
+    { id: 3, type: "Chemical Dept" },
+    { id: 4, type: "Biotech Dept" },
+    { id: 5, type: "Mathmetical Dept" },
+    { id: 6, type: "Managment Dept" }
 ];
